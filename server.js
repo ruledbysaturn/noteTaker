8d7182b51a7536817e7d542f7ee6b1b25e373101
Ruminate <inramiland@gmail.com>
2024-01-09 20:52:18 -0500

    added route to get notes

--- server.js
+++ server.js
@@ -4,3 +4,21 @@ const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+
+const dbFilePath = path.join(__dirname, 'db.json');
+
+//get all notes
+app.get('/api/notes', (req, res) => {
+    fs.readFile(dbFilePath, 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({ error: 'Internal Server Error' });
+        }
+    
+        const notes = JSON.parse(data);
+        res.json(notes);
+      });
+    });
+
