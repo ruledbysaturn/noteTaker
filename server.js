1169ef03233bfcec224d47f2de9b4e3f9fb44bdc
Ruminate <inramiland@gmail.com>
2024-01-09 20:58:55 -0500

    route to save new note added

--- server.js
+++ server.js
@@ -22,3 +22,25 @@ app.get('/api/notes', (req, res) => {
       });
     });
 
+//save new note
+
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    fs.readFile(dbFilePath, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Internal Server Error' });
+      }
+      const notes = JSON.parse(data);
+      newNote.id = Date.now().toString();
+      notes.push(newNote);
+      fs.writeFile(dbFilePath, JSON.stringify(notes), (err) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({ error: 'Internal Server Error' });
+        }
+        res.json(newNote);
+      });
+});
+});
+
